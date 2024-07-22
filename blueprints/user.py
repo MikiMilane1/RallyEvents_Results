46b92fd05d6fd65c2befe69be39e1ed01c1fb3f0
milan <bonkozvogdan@yahoo.com>
2024-07-22 22:45:59 +0200

    Create extensions.py in order to introduce flask_login. Create login, logout and dashboard routes.

--- blueprints/user.py
+++ blueprints/user.py
@@ -5,6 +5,8 @@ from forms import NewUserForm
 from models import TeamModel, UserModel
 from sqlalchemy.exc import IntegrityError
 from passlib.hash import pbkdf2_sha256
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from extensions import login_manager
 
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(APP_PATH, "templates", "user")
@@ -12,9 +14,13 @@ TEMPLATE_PATH = os.path.join(APP_PATH, "templates", "user")
 blp = Blueprint("user", __name__, template_folder=TEMPLATE_PATH)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(UserModel, user_id)
+
+
 @blp.route("/register", methods=["POST", "GET"])
 def register():
-
     form = NewUserForm()
 
     if request.method == 'POST' and form.validate_on_submit():
@@ -22,8 +28,31 @@ def register():
             username=form.username.data,
             password_hash=pbkdf2_sha256.hash(form.password.data)
         )
-        db.session.add(UserModel)
+        db.session.add(user)
         db.session.commit()
-        return render_template('user_info.html', user=user)
+        return render_template('dashboard.html', form=form, user=user)
 
-    return render_template('register.html')
+    return render_template('register.html', form=form)
+
+
+@blp.route("/login", methods=["POST", "GET"])
+def login():
+    form = NewUserForm()
+    if request.method == "POST" and form.validate_on_submit():
+        user = UserModel.query.filter(UserModel.username == form.username.data).first()
+        if user:
+            pass
+
+    form.submit.label.text = 'Login'
+    return render_template('login.html', form=form)
+
+
+@blp.route("/logout", methods=["POST", "GET"])
+def logout():
+    return 'Successfully logged out.'
+
+
+@blp.route("/dashboard/<int:user_id>")
+def dashboard(user_id):
+    user = db.get_or_404(UserModel, user_id)
+    return render_template('dashboard.html', user=user)
--- main.py
+++ main.py
@@ -11,35 +11,36 @@ from functions import register_bps
 
 from blueprints import AddEditEventBP, EventEntryBP, EventBP, SearchBP, HomeBP, EditSSBP, TeamBP, UserBP, DriverBP
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-Bootstrap5(app)
+from extensions import login_manager
 
 
-def configure():
+def create_app():
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+    Bootstrap5(app)
+
+    configure(app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = 'user.login'
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite"
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    register_bps(app, EventBP, DriverBP, EventEntryBP, SearchBP, HomeBP, EditSSBP, AddEditEventBP, TeamBP, UserBP)
+
+    return app
+
+
+def configure(app):
     load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-configure()
-
-# CSRF SECRET KEY
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite"
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-
-register_bps(app, EventBP, DriverBP, EventEntryBP, SearchBP, HomeBP, EditSSBP, AddEditEventBP, TeamBP, UserBP)
-
-
-@app.route('/testing')
-def testing():
-    # return 'testing'
-    form = LoginForm()
-    return render_template('testing.html', form=form)
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
